df9b92744ee18b3f4047e75f61f880354973e639
Kevin Murray <kevinmurray2330@gmail.com>
2025-02-06 13:23:51 -0500

    in new branch: changed welcome message

--- src/main/java/aydin/demo02042025/HelloController.java
+++ src/main/java/aydin/demo02042025/HelloController.java
@@ -9,6 +9,6 @@ public class HelloController {
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome !!!");
+        welcomeText.setText("Welcome to new branch !!!");
     }
 }
